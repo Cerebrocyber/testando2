40d4c22224eccba67bea7d905e30970c6e008c10
Cerebrocyber <132948746+Cerebrocyber@users.noreply.github.com>
2025-04-01 04:13:24 -0300

    Update index.tsx

--- src/pages/cliente1/index.tsx
+++ src/pages/cliente1/index.tsx
@@ -1,1 +1,82 @@
 
+'use client'
+
+import { useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Home = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const play = () => {
+    audioRef.current?.play();
+  };
+
+  const pause = () => {
+    audioRef.current?.pause();
+  };
+
+  const resetAndPlay = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <div className='homepage'>
+  <div className="homepage-background"></div>
+  <section className='homepage-content'>
+
+    <Link href='/' className='flex gap-2 flex-center'>
+      <div className="logo-container">
+        <Image
+          src='/assets/images/imagiluz.svg'
+          alt='logo'
+          fill
+          className='logosmart'
+          style={{ objectFit: 'contain' }}
+        />
+      </div>
+    </Link>
+        
+    {/* <p className="fraselogo">
+          ---
+        </p> */}
+
+    {/*<br className='max-md:hidden' /> */}
+        
+        <Image src='/assets/images/qrcode.svg' alt='' width={120} height={120} className='qrcode' />
+        <p className="qr-text">Escaneie para ouvir no celular!</p>
+
+        {/* Player de áudio */}
+        <audio ref={audioRef} src="/assets/music1.mp3" />
+
+        <div className="player-buttons">
+          <button onClick={play} className="button">
+            <span style={{ fontSize: '22px' }}>▶️</span>
+            <span>PLAY</span>
+          </button>
+
+          <button onClick={pause} className="button">
+            <span style={{ fontSize: '22px' }}>⏸️</span>
+            <span>PAUSE</span>
+          </button>
+
+          <button onClick={resetAndPlay} className="button">
+            <span style={{ fontSize: '22px' }}>🔁</span>
+            <span>RESET</span>
+          </button>
+        </div>
+
+        <footer className="footer">
+          © 2025 Imagiluz. Todos os direitos reservados.
+        </footer>
+
+      </section>
+    </div>
+  );
+};
+
+export default Home;
+
