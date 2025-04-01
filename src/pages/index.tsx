4ca28be07529708ad49bda7e85860091e14f543f
Cerebrocyber <132948746+Cerebrocyber@users.noreply.github.com>
2025-04-01 01:34:51 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,25 +44,7 @@ const Home = () => {
         </p> */}
 
     {/*<br className='max-md:hidden' /> */}
-
-        <div className="bots">
-          <Link href='tel:+5514996217032' className='flex gap-2 flex-center'>
-            <Image src='/assets/images/bx1.svg' alt='' width={60} height={60} className='bot' />
-          </Link>
-          <Link href='https://api.whatsapp.com/send?phone=5514996217032&text=Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20SmartCard' className='flex gap-2 flex-center'>
-            <Image src='/assets/images/bx2.svg' alt='' width={60} height={60} className='bot' />
-          </Link>
-          <Link href='http://www.facebook.com/smartcardweb/' className='flex gap-2 flex-center'>
-            <Image src='/assets/images/bx3.svg' alt='' width={60} height={60} className='bot' />
-          </Link>
-          <Link href='https://www.instagram.com/smartcardweb/' className='flex gap-2 flex-center'>
-            <Image src='/assets/images/bx4.svg' alt='' width={60} height={60} className='bot' />
-          </Link>
-          <Link href='https://goo.gl/maps/J2Y53kaACZYtwgmaA?coh=178573&entry=tt' className='flex gap-2 flex-center'>
-            <Image src='/assets/images/bx5.svg' alt='' width={60} height={60} className='bot' />
-          </Link>
-        </div>
-
+        
         <Image src='/assets/images/qrcode.svg' alt='' width={120} height={120} className='qrcode' />
         <p className="qr-text">Escaneie para ouvir no celular!</p>
 
